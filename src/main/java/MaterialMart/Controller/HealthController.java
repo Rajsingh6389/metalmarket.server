b7d9494539f12c2643bfa802f90fb33f3993e170
Rajsingh6389 <rm2739159@gmail.com>
2026-01-13 15:36:38 +0530

    Add HealthController with health endpoint

--- src/main/java/MaterialMart/Controller/HealthController.java
+++ src/main/java/MaterialMart/Controller/HealthController.java
@@ -1,1 +1,14 @@
 
+package MaterialMart.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
